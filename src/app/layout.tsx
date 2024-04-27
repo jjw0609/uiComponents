b2e21f7c12b8dc8a1a3ec17488533a5c38c308c0
Jung JaeWoong <jjw_0609@naver.com>
2024-04-27 22:28:31 +0900

    기본환경세팅 route 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from 'next'
 import './globals.scss'
-import Gnb from './gnb'
 
 export const metadata: Metadata = {
-  title: 'UI요소모음 | FE재남',
+  title: 'UI요소모음 | JJW',
   description: 'Vanilla / React로 UI요소 만들기',
 }
 
@@ -11,7 +10,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ko">
       <body>
-        <Gnb />
         <main>{children}</main>
       </body>
     </html>
