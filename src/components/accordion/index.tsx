ebf1015d9a6840c51a6316f33ab6f9bf55e21816
Jung JaeWoong <jjw_0609@naver.com>
2024-05-06 18:25:15 +0900

    아코디언, 보이는 요소만 렌더링

---
+++ src/components/accordion/index.tsx
@@ -0,0 +1,11 @@
+import cx from "@/components/accordion/cs";
+
+const Accordions = () => {
+    return (
+        <div className={cx('Accordions')}>
+
+        </div>
+    )
+}
+
+export default Accordions;
